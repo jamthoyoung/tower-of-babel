a855c72e1b1c3ad9f364999c58cb49605f077e3a
James Young <jamthoyoung@gmail.com>
2016-04-11 11:56:27 -0400

    exercise 4

---
+++ ex4exec.js
@@ -0,0 +1,11 @@
+import {PI} from './ex4mod';
+import {sqrt} from './ex4mod';
+import {square} from './ex4mod';
+
+var arg1 = process.argv[2];
+var arg2 = process.argv[3];
+
+console.log(PI);
+console.log(sqrt(+arg1));
+console.log(square(+arg2));
+
