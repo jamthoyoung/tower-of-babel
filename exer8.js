d0d6ce3e2ee56b130bf31e024bf49c92eef47794
James Young <jamthoyoung@gmail.com>
2016-04-13 09:35:28 -0400

    finished exer 8

--- exer8.js
+++ exer8.js
@@ -1,7 +1,24 @@
 const max = process.argv[2];
 let FizzBuzz = {
  [Symbol.iterator]() {
-  
+  let i = 0;
+  return {
+   next() {
+    i++;
+    let is = i;
+    if (i <= max) {
+      let value = i % 15 === 0 ? 'FizzBuzz' :
+          i % 3 === 0 ? 'Fizz' :
+          i % 5 === 0 ? 'Buzz' : i
+     return {
+      done: false,
+      value
+     };
+    } else {
+     return { done: true };
+    }     
+   }
+  }
  }
 }
 
